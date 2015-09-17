8778d79b0451c57f1137466c30a03c4346e30c2b
Liliya Sayfutdinova <windmor@liliyas-mbp.dlink>
2015-09-18 00:15:53 +0500

    Initial Commit

---
+++ GistNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GistNote
+//
+//  Created by Liliya Sayfutdinova on 18/09/15.
+//  Copyright © 2015 Liliya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GistNote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GistNote
+//
+//  Created by Liliya Sayfutdinova on 18/09/15.
+//  Copyright © 2015 Liliya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
